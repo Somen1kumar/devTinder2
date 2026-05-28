487af5809c6ac9cfc96096526208a1b56a987de4
Somen Kumar <somenkumar@Somens-MacBook-Pro.local>
2026-05-28 16:47:57 +0530

    fix URL issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,16 @@
 
 
 
-export const LOGIN_URL = '/auth/login';
+export const LOGIN_URL = '/api/auth/login';
 
-export const GET_PROFILE_DATA_URL = "/profile/userData";
+export const GET_PROFILE_DATA_URL = "/api/profile/userData";
 
-export const LOGOUT_API_URL = "/auth/logout";
+export const LOGOUT_API_URL = "/api/auth/logout";
 
-export const SIGNUP_URL = "/auth/signup";
+export const SIGNUP_URL = "/api/auth/signup";
 
-export const FEED_URL = "/user/feed?page=1&limit=20";
+export const FEED_URL = "/api/user/feed?page=1&limit=20";
 
-export const SENT_CONNECTION = "/request/sent";
+export const SENT_CONNECTION = "/api/request/sent";
 
-export const FEED_FETCH = "/request/review";
+export const FEED_FETCH = "/api/request/review";
